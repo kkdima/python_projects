a63ae5bc650c55911ac516fc233e7a6890e056c4
kkdima <dimaakononenko@gmail.com>
2024-08-14 16:58:11 -0700

    snake is running

---
+++ day_20/snake_game.py
@@ -0,0 +1,28 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+snake = Snake()
+
+
+screen.listen()
+screen.onkey(snake.go_up, "Up")
+screen.onkey(snake.go_down, "Down")
+screen.onkey(snake.go_left, "Left")
+screen.onkey(snake.go_right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+# Keep the game window open
+# screen.mainloop()
