4839e376e8a6b00beb0d3228f260ab7e9b0a02ae
kkdima <dimaakononenko@gmail.com>
2024-08-15 12:40:31 -0700

    paddle move to the separate class

--- day_22/main.py
+++ day_22/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,32 +8,18 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("pink")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(300, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     screen.listen()
-    screen.onkey(go_up, "Up")
-    screen.onkey(go_down, "Down")
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+    screen.onkey(l_paddle.go_up, "w")
+    screen.onkey(l_paddle.go_down, "s")
 
 screen.listen()
 screen.mainloop()
